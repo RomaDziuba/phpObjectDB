bf9c94099d2685277a011cf6bb83cddf0996f1de
Goliath <goliathdp@gmail.com>
2012-05-28 01:45:01 +0300

    Add to PDO getAssoc method

--- ObjectPDOAdapter.php
+++ ObjectPDOAdapter.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Adapter for PDO
@@ -12,52 +12,52 @@ class ObjectPDOAdapter extends ObjectAdapter
     {
         return $this->db->quote($obj, $type);
     }
-    
+
     public function getRow($sql)
     {
         $query = $this->db->prepare($sql);
-        
+
         if($this->db->errorCode() > 0) {
             $info = $this->db->errorInfo();
             throw new DatabaseException($info[2], $info[1]);
         }
-        
+
     	$res = $query->execute();
         if (!$res) {
             $info = $query->errorInfo();
             throw new DatabaseException($info[2], $info[1]);
         }
-        
+
         return $query->fetch();
     }
-    
+
     public function getAll($sql)
     {
         $result = array();
-        
+
         $query = $this->db->prepare($sql);
         if($this->db->errorCode() > 0) {
             $info = $this->db->errorInfo();
             throw new DatabaseException($info[2], $info[1]);
-        } 
+        }
 
         $res = $query->execute();
         if (!$res) {
             $info = $query->errorInfo();
             throw new DatabaseException($info[2], $info[1]);
         }
-        
+
         $result = $query->fetchAll();
-        
+
         return $result;
     }
-    
+
     public function getCol($sql)
     {
         throw new DatabaseException('Undefined method getCol');
     }
-    
-    
+
+
     public function getOne($sql)
     {
         $query = $this->db->prepare($sql);
@@ -67,57 +67,73 @@ class ObjectPDOAdapter extends ObjectAdapter
         }
 
         $res = $query->execute();
-        
+
     	if (!$res) {
         	$info = $query->errorInfo();
             throw new DatabaseException($info[2], $info[1]);
         }
-        
+
         return $query->fetchColumn();
     }
-    
+
     public function getAssoc($sql)
     {
-        throw new DatabaseException('Undefined method getAssoc');
+    	$result = array();
+
+    	$query = $this->db->prepare($sql);
+    	if($this->db->errorCode() > 0) {
+    		$info = $this->db->errorInfo();
+    		throw new DatabaseException($info[2], $info[1]);
+    	}
+
+    	$res = $query->execute();
+    	if (!$res) {
+    		$info = $query->errorInfo();
+    		throw new DatabaseException($info[2], $info[1]);
+    	}
+
+    	$result = $query->fetchAll(PDO::FETCH_KEY_PAIR);
+
+    	return $result;
     }
-    
+
     public function begin($isolationLevel = false)
     {
         // TODO: Savepoint
         //if ($this->db->inTransaction()) {
         //    $this->commit();
         //}
-        
+
         $this->db->beginTransaction();
     }
-    
+
     public function commit()
     {
         $this->db->commit();
     }
-    
+
     public function rollback()
     {
         $this->db->rollBack();
     }
-    
+
     public function query($sql)
     {
         $affected_rows = $this->db->exec($sql);
-        
+
         if($this->db->errorCode() > 0) {
             $info = $this->db->errorInfo();
             throw new DatabaseException($info[2], $info[1]);
         }
-        
+
         return $affected_rows;
     }
-    
+
     protected function getInsertID()
     {
         return $this->getOne("SELECT LAST_INSERT_ID()");
     }
-    
+
 }
 
 ?>
