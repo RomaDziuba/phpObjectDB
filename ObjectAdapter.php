d869c0e5d966e68cf0f0400e5cb22719c11044c3
Denis Panaskin <goliathdp@gmail.com>
2012-07-27 07:56:54 +0100

    Add match command

--- ObjectAdapter.php
+++ ObjectAdapter.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * @abstract
@@ -9,28 +9,28 @@
 abstract class ObjectAdapter implements IObject
 {
     protected $db;
-    
+
     private static $_instances;
-    
+
     protected $reservedWords = array(
-    	'NOW()', 
-        'NOT NULL', 
-        'NULL', 
-        'CURRENT_DATE()', 
+    	'NOW()',
+        'NOT NULL',
+        'NULL',
+        'CURRENT_DATE()',
         'CURRENT_TIME()',
         'CURRENT_DATE',
         'CURRENT_TIME',
         'NOW'
     );
-    
-    public function __construct(&$db) 
+
+    public function __construct(&$db)
     {
         $this->db = $db;
     }
-    
+
     /**
      * Returns objects instance by name
-     * 
+     *
      * @param string $name
      * @return Object
      */
@@ -39,33 +39,33 @@ abstract class ObjectAdapter implements IObject
         if (isset(self::$_instances[$name])) {
             return self::$_instances[$name];
         }
-        
+
         self::$_instances[$name] = Object::getInstance($name, $this->db, $path);
-       
+
         return self::$_instances[$name];
     } // end get
-    
+
     public function insert($table, $values, $is_update_dublicate = false)
     {
         $sql = $this->getInsertSQL($table, $values, $is_update_dublicate);
-        
+
         $this->query($sql);
-        
+
         return $this->getInsertID();
     }
-    
+
     public function delete($table, $condition)
     {
         $where = $this->getSqlCondition($condition);
-        
+
         $sql = "DELETE FROM ".$table." WHERE ".join(" AND ", $where);
-            
+
         return $this->query($sql);
     }
-    
+
     /**
      * Generate an mass insert
-     * 
+     *
      * @param string $table
      * @param mixed $values
      * @throws DatabaseException
@@ -78,12 +78,12 @@ abstract class ObjectAdapter implements IObject
             $data = $this->getInsertValues($items);
             $rows[] = '('.join(', ', $data).')';
         }
-       
+
         $sql = "INSERT INTO ".$table." (".join(", ", array_keys($values[0])).") VALUES ".join(', ', $rows);
-        
+
         return $this->query($sql);
     } // end massInsert
-    
+
     /**
      * Returns quoted values for insert sql
      *
@@ -99,74 +99,74 @@ abstract class ObjectAdapter implements IObject
                 $item = 'NULL';
                 continue;
             }
-            
+
             if ( !in_array($item, $this->reservedWords) ) {
                 $item = $this->quote($item);
             }
         }
         unset($item);
-        
+
         return $values;
     } // end getInsertValues
-    
+
     public function update($table, $values, $condition = array())
     {
         $sql = $this->getUpdateSQL($table, $values, $condition);
-        
+
         return $this->query($sql);
     }
-    
+
     /**
      * Returns the generated SQL query to insert data
-     * 
+     *
      * @param string $table
      * @param array $values
      * @param boolean $is_update_dublicate
      * @return string SQL string
      */
-    public function getInsertSQL($table, $values, $isUpdateDublicate = false) 
+    public function getInsertSQL($table, $values, $isUpdateDublicate = false)
     {
         $values = $this->getInsertValues($values);
-            
+
         $sql = "INSERT INTO ".$table." (".join(", ", array_keys($values)).") VALUES (".join(", ", $values).")";
-            
+
         if ($isUpdateDublicate) {
             $sql .= " ON duplicate KEY UPDATE ";
             $rows = array();
             foreach ($values as $field => $value) {
                 $rows[] = $field." = ".$value;
             }
-                
+
             $sql .= join(", ", $rows);
         }
-            
+
         return $sql;
     } // end getInsertSQL
-    
+
     /**
      * Returns the generated SQL query to update the data
-     * 
+     *
      * @param string $table
      * @param array $values
      * @param string|array $where
      * @return string
      */
-    public function getUpdateSQL($table, $values, $condition = array()) 
+    public function getUpdateSQL($table, $values, $condition = array())
     {
         foreach ($values as $key => &$item) {
-            
+
             if (is_null($item)) {
                 $item = $key." = NULL";
                 continue;
             }
-            
+
             $lastSymbol = mb_substr($key, mb_strlen($key) - 1, 1);
             if (in_array($lastSymbol, array('+', '-'))) {
                 $key = str_replace($lastSymbol, "", $key);
                 $item = $key." = ".$key." ".$lastSymbol." ".$this->quote($item);
                 continue;
             }
-            
+
             if (!$this->reservedWords || !in_array($item, $this->reservedWords)) {
                 $item = $key." = ".$this->quote($item);
             } else {
@@ -174,9 +174,9 @@ abstract class ObjectAdapter implements IObject
             }
         }
         unset($item);
-            
+
         $sql = "UPDATE ".$table." SET ".join(", ", $values);
-        
+
         if (is_array($condition)) {
             $sqlCondition = $this->getSqlCondition($condition);
             if($sqlCondition) {
@@ -185,41 +185,42 @@ abstract class ObjectAdapter implements IObject
         } else {
             $sql .= " WHERE ".$condition;
         }
-            
+
         return $sql;
     } // end getUpdateSQL
-    
-    public function getSqlCondition($obj = array(), $is_default = true) 
+
+    // FIXME: Need refactoring this
+    public function getSqlCondition($obj = array(), $is_default = true)
     {
         $result = $is_default ? array('TRUE') : array();
-        
+
         foreach ($obj as $key => $item) {
             $buffer = explode("&", $key);
             $action = !empty($buffer[1]) ? $buffer[1] : "=";
-            
+
             if (in_array($action, array('IN', 'NOT IN'))) {
                 $values = array();
                 if (!$item) {
                     continue;
                 }
-                $item = is_scalar($item) ? array($item) : $item;                
+                $item = is_scalar($item) ? array($item) : $item;
                 foreach($item as $val) {
                     $values[] = $this->quote($val);
                 }
-                
+
                 if($values) {
                     $result[] = $buffer[0]." ".$action." (".join(', ', $values).')';
                 }
-                continue; 
+                continue;
             }
-            
+
             if(strtolower($action) == 'or_sql') {
                 $result[] = '('.join(' OR ', $item).')';
                 continue;
             }
-            
+
             if ($key == 'sql_or') {
-                
+
                 $search = array();
                 foreach ($item as $row) {
                     if (is_scalar($row)) {
@@ -228,13 +229,13 @@ abstract class ObjectAdapter implements IObject
                         $search[] = join(' AND ', $this->getSqlCondition($row, false));
                     }
                 }
-                
+
                 $result[] = '(('.join(' ) OR (', $search).'))';
                 continue;
             }
-            
+
             if ($key == 'sql_and') {
-                
+
                 $search = array();
                 foreach ($item as $row) {
                     if (is_scalar($row)) {
@@ -243,38 +244,43 @@ abstract class ObjectAdapter implements IObject
                         $search[] = join(' AND ', $this->getSqlCondition($row, false));
                     }
                 }
-                
+
                 $result[] = join(' AND ', $search);
                 continue;
             }
-            
-            
-            if (strtolower($action) == 'or') {
-                
+
+            $command = strtolower($action);
+
+            if ($command == 'or') {
+
                 list($value, $others) = $item;
-                
-                $action = empty($buffer[2]) ? '&=' : '&'.$buffer[2]; 
-                
+
+                $action = empty($buffer[2]) ? '&=' : '&'.$buffer[2];
+
                 $condition = array($buffer[0].$action => $value);
                 $ors = $this->getSqlCondition($condition, false);
-                
+
                 $others = $this->getSqlCondition($others, false);
                 $conditions = array_merge($ors, $others);
                 $result[] = '('.join(' OR ', $conditions).')';
-                
+
                 continue;
+            } else if ($command == "match") {
+
+               $result[] = "MATCH (".$buffer[0].") AGAINST (".$this->quote($item).")";
+               continue;
             }
-            
+
             if (!in_array($item, $this->reservedWords)) {
-                $result[] = $buffer[0]." ".$action." ".$this->quote($item); 
+                $result[] = $buffer[0]." ".$action." ".$this->quote($item);
             } else {
                 $result[] = $buffer[0]." ".$action." ".$item;
             }
         }
-        
+
         return $result;
     } // end getSqlCondition
-    
-    
+
+
 }
 ?>
